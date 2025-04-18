ea37e645bb1739709a0021568baf8084f9c2f2fa
NureAlam68 <nurealam151068@gmail.com>
2025-04-18 12:57:43 +0600

    searching

---
+++ B_Searching.c
@@ -0,0 +1,25 @@
+// Given a number N and an array A of N numbers. Determine if the number X exists in array A or not and print its position (0-index).
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int x;
+    scanf("%d", &x);
+    int found = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+        {
+            found = i;
+            break;
+        }
+    }
+    printf("%d", found);
+    return 0;
+}
